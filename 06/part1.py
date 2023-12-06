462d78127e144d481c7f9be1982865c03cc4c604
QuantumCucumber <quantumcucumber@protonmail.com>
2023-12-06 18:30:34 +1100

    Day 6 ???

---
+++ 06/part1.py
@@ -0,0 +1,6 @@
+# I just plugged the values into desmos and counted the points lol
+# (x, x(t-x)) for x = [0,...100]
+# y=s
+# t=TIME
+# s=DISTANCE
+print(26*22*19*71)
---
+++ 06/part2.py
@@ -0,0 +1,11 @@
+# This wasnt much harder except that I am definetely not counting
+# Just simultaneous equations
+
+# x(41777096-x)=249136211271011
+# -x^2+41777096x-249136211271011=0
+# Quadratic formula gives:
+# x = ~7206617
+# x = ~34570479
+# Since 7206617 is the lower bound, it will most likely contain a decimal, so round up to 7206618
+
+print(34570479-7206618)
